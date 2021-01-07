2dfa6d4662f2ee4526f9740238f9aa459b2a3a92
Sean <itsmesean@pm.me>
2021-01-06 18:36:56 -0800

    default dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,39 +1,41 @@
 module.exports = {
-  title: 'reVerb docs',
-  tagline: 'The tagline of my site',
-  url: 'https://docs.reverb-vs.com/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/miniLogo.svg',
-  organizationName: 'reVerb-LLC', 
-  projectName: 'reverb-docs',
+  title: "reVerb docs",
+  tagline: "The tagline of my site",
+  url: "https://reVerb-LLC.github.io",
+  baseUrl: "/reverb-docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/miniLogo.svg",
+  organizationName: "reVerb-LLC",
+  projectName: "reverb-docs",
   themeConfig: {
+    colorMode: {
+      defaultMode: "dark",
+    },
     sidebarCollapsible: false,
     navbar: {
-      title: 'Docs',
+      title: "Docs",
       logo: {
-        alt: 'reVerb Logo',
-        src: 'img/reVerbLogo.svg',
+        alt: "reVerb Logo",
+        src: "img/reVerbLogo.svg",
       },
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} reVerb LLC. Built with Docusaurus.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/reVerb-LLC/reverb-docs',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          editUrl: "https://github.com/reVerb-LLC/reverb-docs",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
